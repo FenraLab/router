82834214a3ce8086eb30739e25104a6e66cbf873
James R Swift <james.rt.swift@gmail.com>
2025-11-10 21:58:32 +0000

    Corrections to configuration

--- src/decorators.ts
+++ src/decorators.ts
@@ -4,7 +4,7 @@ import { Request, Response } from "./types"
 import { Endpoint } from "./routing"
 
 export function declareHandler(method: string) {
-    return function (target: Middleware.TFunction<[Request, Response]>, context: ClassMethodDecoratorContext<Endpoint>) {
+    return function (target: any, context: ClassMethodDecoratorContext) {
         context.addInitializer(function RegisterDeclaredHandler(this: Endpoint) {
             this.handle(method, this[target.name].bind(this))
         })  
